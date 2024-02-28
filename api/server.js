92a66a8469a6c443cb3cad9c10328c652349c3ff
Your Name <you@example.com>
2024-02-28 23:38:38 +0100

    .

--- api/server.js
+++ api/server.js
@@ -1,30 +1,29 @@
 const jsonServer = require('json-server');
 
 const server = jsonServer.create();
-
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
-// Add this before server.use(router)
+// Set the Access-Control-Allow-Origin header to allow any origin
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173, http://localhost:5174');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+  '/api/*': '/$1',
+  '/blog/:resource/:id/show': '/:resource/:id'
 }));
 
 server.use(router);
 
 const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server is running on port ${PORT}`);
 });
 
 // Export the Server API
