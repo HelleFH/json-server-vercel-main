6e65228b623fd2914a20ca2f9b737cbf3e64abd2
Your Name <you@example.com>
2024-02-28 23:30:16 +0100

    .

--- api/server.js
+++ api/server.js
@@ -1,31 +1,31 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
 
-const server = jsonServer.create()
+const server = jsonServer.create();
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
+server.use(middlewares);
 
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
 // Add this before server.use(router)
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173, http://localhost:5174');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+}));
+
+server.use(router);
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`);
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
